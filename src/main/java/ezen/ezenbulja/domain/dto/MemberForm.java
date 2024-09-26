2d47df0bb71d0b0be1d5f6598bffdb689260480a
han sunfeel <seonbinha44@gmail.com>
2024-09-26 21:35:28 +0900

    로그인 수정

--- src/main/java/ezen/ezenbulja/domain/dto/MemberForm.java
+++ src/main/java/ezen/ezenbulja/domain/dto/MemberForm.java
@@ -15,7 +15,7 @@ public class MemberForm {
     @NotEmpty(message = "이름을 입력해주세요.")
     private String name;
 
-    @NotEmpty(message = "현재 비밀번호를 입력해주세요.")
+    @NotEmpty(message = "사용할 비밀번호를 입력해주세요.")
     private String password;  // 현재 비밀번호로 수정
 
     private String newPassword;  // 새 비밀번호
